de670ea888de1886a5d33e67576f21c3205b11a5
raghdarizq <raghdarizq2@gmail.com>
2023-05-17 11:18:39 +0300

    make header sec Relets #8

--- src/App.js
+++ src/App.js
@@ -6,16 +6,23 @@ import Images from './components/Images';
 export default class App extends Component {
   constructor() {
     super();
-    this.state = { inputValue: 'red flower' };
+    this.state = {
+      query: 'red flower',
+      type: "all"
+    };
+    
   }
+  setData =(newQuery)=>{
+    this.setState({query:newQuery })
+}
 
   render() {
     return (
       <>
         <div className="App">
           <header className="App-header">
-            <Header />
-            <Images type="all" query="yellow flowers" />
+            <Header  type={this.state.type} query={this.state.query} setData={this.setData}/>
+            <Images type={this.state.type} query={this.state.query} />
           </header>
         </div>
       </>
